0e7b11b489355ba47313778efce11091f8a7936d
GalJan <ga82li@gmail.com>
2021-09-21 12:34:42 +0200

    Druha class Adresa

--- BankIS/BankIS/BankIS.ConsoleApp/Program.cs
+++ BankIS/BankIS/BankIS.ConsoleApp/Program.cs
@@ -8,15 +8,27 @@ namespace BankIS.ConsoleApp
         {
             Console.WriteLine("Hello World!");
 
+            Address ostrava = new Address();
+            ostrava.City = "Ostrava";
+            ostrava.Street = "Tovarni 13";
+
             Client client1=new Client();
 
             client1.Name = "Jan Novak";
             client1.Age = 31;
+            client1.HomeAddress = new Address();
+            client1.HomeAddress.Street = "Sochorova 23";
+            client1.HomeAddress.City = "Brno";
 
             Client client2 = new Client();
 
             client2.Name = "Petr Skocdopole";
             client2.Age = 25;
+            client2.HomeAddress = new Address();
+            client2.HomeAddress.Street = "2. Ulice";
+            client2.HomeAddress.City = "Praha";
+
+            
 
             client1.Print();
             client2.Print();
