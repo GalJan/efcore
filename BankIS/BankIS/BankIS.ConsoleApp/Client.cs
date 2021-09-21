09d4a1a6ba9d3fb5c302c1e45e57e2cbb9264620
GalJan <ga82li@gmail.com>
2021-09-21 14:42:28 +0200

    konstruktor pro vse

--- BankIS/BankIS/BankIS.ConsoleApp/Client.cs
+++ BankIS/BankIS/BankIS.ConsoleApp/Client.cs
@@ -8,17 +8,44 @@ namespace BankIS.ConsoleApp
 {
     public class Client
     {
+
+        public Client()
+        {
+            HomeAddress = new Address();
+
+        }
+        public Client(string street, string city)
+        {
+            HomeAddress = new Address();
+            HomeAddress.Street = street;
+            HomeAddress.City = city;    
+        }
+        public Client(string name, string street, string city)
+        {
+            HomeAddress = new Address();   
+            Name = name;
+            HomeAddress.Street = street;   
+            HomeAddress.City = city;    
+        }
         public string Name { get; set; }
 
         public int Age { get; set; }
 
-        public Address HomeAddress {  get; set; }   
+        public Address HomeAddress {  get; set; }
 
         public void Print(string street, string city)
         {
-              
+
             Console.WriteLine(Name);
-            Console.WriteLine("{0},{1}",street,city);
+            
+            if (HomeAddress != null && !string.IsNullOrEmpty(HomeAddress.Street))
+            {
+                Console.WriteLine("{0}, {1}", street, city);
+            }
+            else
+            {
+                Console.WriteLine("Adresa nezadana!");
+            }
             //Console.WriteLine(HomeAddress.City);
         }
     }
--- BankIS/BankIS/BankIS.ConsoleApp/Program.cs
+++ BankIS/BankIS/BankIS.ConsoleApp/Program.cs
@@ -8,30 +8,33 @@ namespace BankIS.ConsoleApp
         {
             Console.WriteLine("Hello World!");
 
-            Address ostrava = new Address("Tovarni 13", "Ostrava");
+            //Address ostrava = new Address("Tovarni 13", "Ostrava");
 
-            ostrava.City = "Ostrava";
-            ostrava.Street = "Tovarni 13";
+            //ostrava.City = "Ostrava";
+            //ostrava.Street = "Tovarni 13";
 
-            Client client1=new Client();
+            Client client1=new Client("Machova 126","Machova");
 
             client1.Name = "Jan Novak";
             client1.Age = 31;
-            client1.HomeAddress = ostrava;
+
+            Client client2 = new Client("Jan Gala", "Machova 126", "Myslocovice");
+            //client1.HomeAddress = ostrava;
             //client1.HomeAddress.Street = "Sochorova 23";
             //client1.HomeAddress.City = "Brno";
 
-            Client client2 = new Client();
+            //Client client2 = new Client();
 
-            client2.Name = "Petr Skocdopole";
-            client2.Age = 25;
-            client2.HomeAddress = new Address("","");
-            client2.HomeAddress.Street = "2. Ulice";
-            client2.HomeAddress.City = "Praha";
+            //client2.Name = "Petr Skocdopole";
+            //client2.Age = 25;
+            //client2.HomeAddress = new Address("2.Ulice","Praha");
+            //client2.HomeAddress.Street = "2. Ulice";
+            //client2.HomeAddress.City = "Praha";
 
             
 
             client1.Print(client1.HomeAddress.Street,client1.HomeAddress.City);
+            Console.WriteLine();
             client2.Print(client2.HomeAddress.Street, client2.HomeAddress.City);
         }
 
