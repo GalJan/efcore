e55a00c50ec82714e0dea3f26c013cf0f6a30919
GalJan <ga82li@gmail.com>
2021-09-21 12:11:27 +0200

    Class a Instance

---
+++ BankIS/BankIS/BankIS.ConsoleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankIS.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Client client1=new Client();
+
+            client1.Name = "Jan Novak";
+            client1.Age = 31;
+
+            client1.Print();
+        }
+    }
+}
